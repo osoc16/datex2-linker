6c024397f2573c52cd6642b0604e5a7eae466337
Haroen Viaene <hello@haroen.me>
2016-07-13 21:12:37 +0200

    parse on every request
    Since this should work well with realtime requests as well, we have to parse on every request. This requires the tests of `serve` to be rewritten as well

--- test/test-serve.js
+++ test/test-serve.js
@@ -2,19 +2,23 @@ import test from 'ava';
 import url from 'url';
 import got from 'got';
 import serve from '../lib/serve.js';
+import http from 'http';
+
+const server = http.createServer(function(req, res) {
+  res.end(`
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<string someremovednamespace:attribute="value">
+<text>content</text>
+</string>`);
+});
+server.listen('8000');
 
 test('starts serving', t => {
-  serve({
-    something: 'its value',
-    somethingElse: 'with value'
-  }, url.parse('http://localhost:4002'), 'title', true);
+  serve('http://localhost:8000/', url.parse('http://localhost:4002'), 'title', true);
 });
 
 test('serves an index', async t => {
-  await serve({
-    something: 'its value',
-    somethingElse: 'with value'
-  }, url.parse('http://localhost:4003'), 'title', true);
+  await serve('http://localhost:8000/', url.parse('http://localhost:4003'), 'title', true);
   const response = await got('http://localhost:4003').then(res => {
     return Promise.resolve(res.body);
   });
@@ -29,10 +33,7 @@ code {
 });
 
 test('content type of data.json is application/json', async t => {
-  await serve({
-    something: 'its value',
-    somethingElse: 'with value'
-  }, url.parse('http://localhost:4004'), 'title', true);
+  await serve('http://localhost:8000/', url.parse('http://localhost:4004'), 'title', true);
   const contentType = await got('http://localhost:4004/data.json').then(res => {
     return Promise.resolve(res.headers['content-type']);
   });
@@ -40,10 +41,7 @@ test('content type of data.json is application/json', async t => {
 });
 
 test('content type of data.jsonld is application/ld+json', async t => {
-  await serve({
-    something: 'its value',
-    somethingElse: 'with value'
-  }, url.parse('http://localhost:4005'), 'title', true);
+  await serve('http://localhost:8000/', url.parse('http://localhost:4005'), 'title', true);
   const contentType = await got('http://localhost:4005/data.jsonld').then(res => {
     return Promise.resolve(res.headers['content-type']);
   });
