8c5d1f178f3a3b7a591158acce586ccf9aba9b6a
Haroen Viaene <hello@haroen.me>
2016-07-13 07:58:59 +0200

    test serve.js

--- test/test.js
+++ test/test.js
@@ -1,5 +1,7 @@
 import test from 'ava';
 import http from 'http';
+import url from 'url';
+import got from 'got';
 import parse from '../lib/parse.js';
 import serve from '../lib/serve.js';
 
@@ -38,9 +40,51 @@ test('served xml converts id to @id', async t => {
   t.is(json, '{"@context":{"@base":"http://vocab.datex.org/terms#"},"data":{"d2LogicalModel":{"xmlns":"//datex2.eu/schema/2/2_0","modelBaseVersion":2,"@id":"http://test.dev/datex/data.jsonld#azertyuiop"}}}');
 });
 
-test('serves up an index', t => {
+test('starts serving', t => {
   serve({
     something: 'its value',
     somethingElse: 'with value'
+  }, url.parse('http://localhost:4002'), 'title');
+});
+
+test('serves an index', async t => {
+  await serve({
+    something: 'its value',
+    somethingElse: 'with value'
+  }, url.parse('http://localhost:4003'), 'title');
+  const response = await got('http://localhost:4003').then(res => {
+    return Promise.resolve(res.body);
   });
+  // todo: change this when the index changes looks
+  t.is(response,
+    `<!DOCTYPE html><html lang="en"><head><title>title</title><style>body {
+  font-family: -apple-system, sans-serif;
+}
+code {
+  font-family: Menlo, monospace;
+}</style></head><body><h1>title</h1><p>Generated via <a href="https://github.com/osoc16/datex2-linker">datex2-linker</a></p><p>You can find the <code>json-ld</code> at <a href="/data.json">/data.json</a> or <a href="/data.jsonld">/data.jsonld</a></p><blockquote>TODO: make this into a better landing page that fits in with moby link</blockquote></body></html>`)
+});
+
+test('content type of data.json is application/json', async t => {
+  await serve({
+    something: 'its value',
+    somethingElse: 'with value'
+  }, url.parse('http://localhost:4004'), 'title');
+  const contentType = await got('http://localhost:4004/data.json').then(res => {
+    // todo: return content type
+    return Promise.resolve(res);
+  });
+  t.is(contentType, 'application/json');
+});
+
+test('content type of data.jsonld is application/ld+json', async t => {
+  await serve({
+    something: 'its value',
+    somethingElse: 'with value'
+  }, url.parse('http://localhost:4004'), 'title');
+  const contentType = await got('http://localhost:4004/data.jsonld').then(res => {
+    // todo: return content type
+    return Promise.resolve(res);
+  });
+  t.is(contentType, 'application/ld+json');
 });
